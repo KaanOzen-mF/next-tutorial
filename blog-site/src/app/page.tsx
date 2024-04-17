fd4dd7f5de8bbd9fa7c2f018978e09b33f59b1b8
KaanOzen-mF <kaanozn98@gmail.com>
2024-04-17 16:22:14 +0300

    main page and layout change

--- blog-site/src/app/page.tsx
+++ blog-site/src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Kaan</span>
+        </span>
+      </p>
     </main>
   );
 }
