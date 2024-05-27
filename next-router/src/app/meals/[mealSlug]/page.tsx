c098eb70484645b0e904b10d28742310082db5ae
KaanOzen-mF <kaanozn98@gmail.com>
2024-05-27 12:27:05 +0300

    feat: Update MetalDetailsPage component to fix parameter passing issue

--- next-router/src/app/meals/[mealSlug]/page.tsx
+++ next-router/src/app/meals/[mealSlug]/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+//In this function, normally have {params} inside the parentheses
+//But next js is know that we are passing the params object
+
 export default function MetalDetailsPage() {
   return (
     <div className="text-center">
