455e2062ba5a757b3a00a4daa508bc9f5035be73
KaanOzen-mF <kaanozn98@gmail.com>
2024-05-27 12:24:12 +0300

    feat: Add Share page for meals

---
+++ next-router/src/app/meals/share/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Share() {
+  return (
+    <div className="text-center">
+      <h1>Share Page</h1>
+    </div>
+  );
+}
