c78144288e91d4ce3cb563bd4af56de06b89d984
KaanOzen-mF <kaanozn98@gmail.com>
2024-05-27 12:23:54 +0300

    feat: Add Meals page

---
+++ next-router/src/app/meals/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Meals() {
+  return (
+    <div className="text-center">
+      <h1>Meals Page</h1>
+    </div>
+  );
+}
