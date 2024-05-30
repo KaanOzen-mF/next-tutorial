dc81906aaafa86c7a503daa62b3af3a5b2380d8f
KaanOzen-mF <kaanozn98@gmail.com>
2024-05-30 16:11:04 +0300

    feat: Update Meals page with header and link to share favorite recipe
    This commit adds a header to the Meals page, displaying a title and a description. It also includes a link to the "Share Your Favorite Recipe" page. These changes enhance the visual appeal and user experience of the Meals page, making it more engaging and encouraging users to share their recipes.

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,7 +1,33 @@
+import MealsGrid from "@/components/mealsGrid";
+import Link from "next/link";
+
 export default function Meals() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className="gap-12 mt-12 mr-auto mb-20 ml-auto w-11/12 max-w-6xl text-2xl text-center">
+        <h1>
+          Delicious meals, created{" "}
+          <span className="bg-gradient-to-r-from-orange-to-yellow bg-clip-text text-transparent">
+            by you
+          </span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p>
+          <Link
+            href="/meals/share"
+            className="inline-block mt-4 py-2 px-4 rounded-lg bg-gradient-to-r-from-orange-to-yellow
+            text-white font-bold "
+          >
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main>
+        <MealsGrid meals={[]} />
+      </main>
+      ;
+    </>
   );
 }
