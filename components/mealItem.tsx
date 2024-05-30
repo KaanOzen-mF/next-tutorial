a36b67aed77e28dbfd95faf9c30a4e0ffe94c6e3
KaanOzen-mF <kaanozn98@gmail.com>
2024-05-30 17:32:19 +0300

    feat: Update MealItem component with responsive layout and improved styling
    This commit modifies the MealItem component to include a responsive layout using Tailwind CSS classes. It also improves the styling by adding a gradient background and adjusting the positioning of the image and text elements. These changes enhance the visual appeal and user experience of the MealItem component.

--- components/mealItem.tsx
+++ components/mealItem.tsx
@@ -17,20 +17,25 @@ export default function MealItem({
   slug,
 }: MealItemProps) {
   return (
-    <article>
+    <article className="flex flex-col justify-between h-full rounded shadow-md overflow-hidden bg-gradient-to-r-from-orange-to-yellow">
       <header>
-        <div>
-          <Image src={image} alt={title} fill />
+        <div className=" relative h-60">
+          <Image src={image} alt={title} className=" object-cover" fill />
         </div>
-        <div>
-          <h2>{title}</h2>
-          <p>by {creator}</p>
+        <div className="pt-2 pr-4 pb-0 pl-4">
+          <h2 className="m-0 text-xl font-sans">{title}</h2>
+          <p className="m-0 text-sm italic">by {creator}</p>
         </div>
       </header>
-      <div>
-        <p>{summary}</p>
-        <div>
-          <Link href={`/meals/${slug}`}>View Details</Link>
+      <div className="flex flex-col justify-between h-ful">
+        <p className="pt-4 pl-4 pb-0 pr-4">{summary}</p>
+        <div className="p-4 text-right">
+          <Link
+            href={`/meals/${slug}`}
+            className="inline-block mt-4 py-2 px-4 rounded-lg bg-gradient-to-r-from-orange-to-yellow font-bold"
+          >
+            View Details
+          </Link>
         </div>
       </div>
     </article>
