f1ce54b6893ad927e9edd3dec78930b3813a352b
KaanOzen-mF <kaanozn98@gmail.com>
2024-05-31 13:37:40 +0300

    refactor: Add NotFound component for displaying "Not Found" page
    This commit adds a new component, NotFound, which displays a "Not Found" page when the requested page is not found. The component renders a main section with a large heading and a paragraph explaining that the requested page could not be found. This improves the user experience by providing a clear and informative message when navigating to non-existent pages.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+export default function NotFound() {
+  return (
+    <main className="mt-20 text-center">
+      <h1
+        className="text-7xl m-0 font-black uppercase
+    bg-gradient-to-r-from-orange-to-yellow bg-clip-text text-transparentl bg-cover bg-center"
+      >
+        Not Found!
+      </h1>
+      <p className="text-2xl font-medium text-white">
+        Unfortunately, we could not find the requested page
+      </p>
+    </main>
+  );
+}
