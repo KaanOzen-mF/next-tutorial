d4d0717812ea5b8bd18dc3bed4fdf83bd17c7b42
ceng3011 <ceng3011@gmail.com>
2024-06-10 14:37:55 +0000

    getMeal function add

--- lib/meals.ts
+++ lib/meals.ts
@@ -3,6 +3,22 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); //extra delay to simulate a slow network
+  //await new Promise((resolve) => setTimeout(resolve, 2000)); //extra delay to simulate a slow network
   return db.prepare("SELECT * FROM meals").all();
+}
+type Meal = {
+  id: number;
+  title: string;
+  slug: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creatorEmail: string;
+};
+
+export function getMeal(slug: string): Meal | undefined {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug) as
+    | Meal
+    | undefined;
 }
