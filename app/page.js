6d161b7a842116aae0aae4eee26076ccc90e4076
ceng3011 <ceng3011@gmail.com>
2024-07-23 12:23:45 +0000

    page route add

--- app/page.js
+++ app/page.js
@@ -3,9 +3,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-center text-white">
+        Hello world!
+      </h1>
       <p>
         <Link href={"/about"}>About Us</Link>
+      </p>
+      <p>
+        <Link href={"/meals/share"}>Share Meal</Link>
+      </p>
+      <p>
+        <Link href={"/community"}>Community</Link>
       </p>
     </main>
   );
