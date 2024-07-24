5c44fe15b6fd51da1908e365ba3cd77de589ac72
ceng3011 <ceng3011@gmail.com>
2024-07-24 07:09:50 +0000

    next Image comp add

--- components/main-header.js
+++ components/main-header.js
@@ -1,24 +1,26 @@
 import Link from "next/link";
 
 import logoImg from "@/assests/logo.png";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className="flex justify-between items-center py-8 px-4 md:px-[10%]">
       <Link
         href="/"
-        className="flex items-center justify-center gap-8 text-logoColor font-bold uppercase tracking-widest text-xl md:text-2xl"
+        className="flex items-center justify-center gap-8 text-logoColor font-bold uppercase tracking-widest text-xl md:text-2xl text-[#ddd6cb]"
       >
-        <img
-          src={logoImg.src}
+        <Image
+          src={logoImg}
           alt="A plate with food on it"
+          priority
           className="w-20 h-20 object-contain drop-shadow-[0_0_0.75rem_rgba(0,0,0,0.5)]"
         />
         NextLevel Food
       </Link>
 
       <nav>
-        <ul className="flex gap-6 list-none m-0 p-0 text-lg md:text-xl">
+        <ul className="flex gap-6 list-none m-0 p-0 text-lg md:text-xl text-[#ddd6cb]">
           <li>
             <Link
               href="/meals"
