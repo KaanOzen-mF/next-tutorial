0e0500462f2002ffbf67968d31a555143e9a1295
ceng3011 <ceng3011@gmail.com>
2024-07-24 06:05:39 +0000

    main header stlye add | unusued element remove from page

--- app/page.js
+++ app/page.js
@@ -3,9 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline text-center text-white">
-        Hello world!
-      </h1>
       <p>
         <Link href={"/about"}>About Us</Link>
       </p>
--- components/main-header.js
+++ components/main-header.js
@@ -4,19 +4,36 @@ import logoImg from "@/assests/logo.png";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="Logo" />
+    <header className="flex justify-between items-center py-8 px-4 md:px-[10%]">
+      <Link
+        href="/"
+        className="flex items-center justify-center gap-8 text-logoColor font-bold uppercase tracking-widest text-xl md:text-2xl"
+      >
+        <img
+          src={logoImg.src}
+          alt="A plate with food on it"
+          className="w-20 h-20 object-contain drop-shadow-[0_0_0.75rem_rgba(0,0,0,0.5)]"
+        />
         NextLevel Food
       </Link>
 
       <nav>
-        <ul>
+        <ul className="flex gap-6 list-none m-0 p-0 text-lg md:text-xl">
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <Link
+              href="/meals"
+              className="text-logoColor font-bold py-2 px-4 rounded hover:bg-gradient-to-r from-[#ff8a05] to-[#f9b331] hover:bg-clip-text hover:text-transparent hover:shadow-[0_0_18px_rgba(248,190,42,0.8)]"
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <Link
+              href="/community"
+              className="text-logoColor font-bold py-2 px-4 rounded hover:bg-gradient-to-r from-[#ff8a05] to-[#f9b331] hover:bg-clip-text hover:text-transparent hover:shadow-[0_0_18px_rgba(248,190,42,0.8)]"
+            >
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
