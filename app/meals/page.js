154fc3f3d89e59fbdf5933520b0d394d06fff855
ceng3011 <ceng3011@gmail.com>
2024-07-29 09:19:10 +0000

    add fetch data to meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,28 @@
-export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+  return (
+    <>
+      <header className="w-full mx-auto max-w-[90rem] gap-24 mt-24 mb-20 text-[#ddd6cb] text-2xl leading-loose">
+        <h1>
+          Delicious meals, created{" "}
+          <span className="bg-gradient-to-r from-[#f9572a] to-[#ff8a05] bg-clip-text text-transparent">
+            by you
+          </span>
+        </h1>
+        <p className="m-0">
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className="inline-block mt-4 px-2 py-4 rounded-lg font-bold bg-gradient-to-r from-[#f9572a] to-[#ff9b05]">
+          <Link href={"/meals/share"}>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 }
