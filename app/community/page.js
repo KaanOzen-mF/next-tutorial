da32e3971fc58840d2f64c947c8df87aafb1cc23
ceng3011 <ceng3011@gmail.com>
2024-07-29 14:46:04 +0000

    community page create

--- app/community/page.js
+++ app/community/page.js
@@ -1,3 +1,56 @@
+import Image from "next/image";
+
+import mealIcon from "@/assests/icons/meal.png";
+import communityIcon from "@/assests/icons/community.png";
+import eventsIcon from "@/assests/icons/events.png";
+
 export default function CommunityPage() {
-  return <h1>Community Page</h1>;
+  return (
+    <>
+      <header className="gap-24 mt-24 mb-20 mx-auto w-11/12 max-w-6xl text-[#ddd6cb] text-2xl text-center">
+        <h1>
+          One shared passion:{" "}
+          <span className="bg-gradient-to-r from-[#ff8a05] to-[#f9b331] bg-clip-text text-transparent">
+            Food
+          </span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+      </header>
+      <main className="w-11/12 max-w-xl my-0 mx-auto text-center">
+        <h2 className="text-xl mb-12 text-[#ddd6cb]">Community Perks</h2>
+        <ul className="p-0 mx-12 my-0">
+          <li className="flex flex-col items-center gap-8">
+            <Image
+              className="w-32 h-32 object-contain"
+              src={mealIcon}
+              alt="A delicious meal"
+            />
+            <p className="text-[#ddd6cb] text-2xl font-bold m-0">
+              Share & discover recipes
+            </p>
+          </li>
+          <li className="flex flex-col items-center gap-8">
+            <Image
+              className="w-32 h-32 object-contain"
+              src={communityIcon}
+              alt="A crowd of people, cooking"
+            />
+            <p className="text-[#ddd6cb] text-2xl font-bold m-0">
+              Find new friends & like-minded people
+            </p>
+          </li>
+          <li className="flex flex-col items-center gap-8">
+            <Image
+              className="w-32 h-32 object-contain"
+              src={eventsIcon}
+              alt="A crowd of people at a cooking event"
+            />
+            <p className="text-[#ddd6cb] text-2xl font-bold m-0">
+              Participate in exclusive events
+            </p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 }
