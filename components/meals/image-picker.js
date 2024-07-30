a30ca5b04bc5d56dbc7fa1743216dcf23e00e0e4
ceng3011 <ceng3011@gmail.com>
2024-07-30 14:15:05 +0000

    image picker image preview add

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,25 +1,55 @@
 "use client";
-import { useRef } from "react";
+import Image from "next/image";
+import { useRef, useState } from "react";
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   function handlePickClick() {
     imageInput.current.click();
   }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+  
   return (
-    <div className=" hidden border-0 px-2 py-6 bg-[#a4abb9] rounded-sm cursor-pointer hover:bg-[#b3b9c6] focus:bg-[#b3b9c6]">
+    <div>
       <label htmlFor={name}>{label}</label>
-      <div className="flex items-start gap-6 mb-4">
+      <div className="controls flex items-start gap-6 mb-4">
+        <div className="preview w-40 h-40 border-2 border-gray-400 flex justify-center items-center text-center text-gray-400 relative">
+          {!pickedImage && <p className="m-0 p-4">No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
         <input
-          className="hidden"
+          className="input hidden"
           type="file"
           id={name}
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
-          className="border-0 px-2 py-6 bg-[#a4abb9] rounded-sm cursor-pointer hover:bg-[#b3b9c6] focus:bg-[#b3b9c6]"
+          className="button border-0 py-2 px-6 bg-gray-400 rounded cursor-pointer font-inherit hover:bg-gray-500 focus:bg-gray-500"
           type="button"
           onClick={handlePickClick}
         >
