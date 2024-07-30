025f42899e9faf6bd6814acfb8386a9af64b95e2
ceng3011 <ceng3011@gmail.com>
2024-07-30 06:30:08 +0000

    not found page add

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <main className="mt-64 text-center tracking-wider">
+      <h1 className="m-0 text-7xl font-extrabold uppercase bg-gradient-to-r from-[#f9572a] to-[#ff8a05] bg-clip-text text-transparent">
+        Not Found
+      </h1>
+      <p className="text-[#ddd8d8] font-bold text-xl mt-12">
+        Unfortunately, we could not find the requested page or resource
+      </p>
+    </main>
+  );
+}
